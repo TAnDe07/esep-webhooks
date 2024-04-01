e0fb13cdd39fb8e1c3aa10a7f5e4ea42976ef08a
TAnDe07 <tdesir1@ufl.edu>
2024-03-31 23:35:09 -0400

    update function.cs

--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7f0d0314cf4975ae472ceee8f13d4e6a9041d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8e09aa03883bec049e429ecc50172c0f8db80b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
